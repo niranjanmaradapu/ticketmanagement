1bac1a11cf9059ba45947b6d6adb04627e0a50da
Sudheer <sudheer.swamy@otsi.co.in>
2022-09-01 17:14:21 +0530

    clientId related issues fixed

--- src/main/java/com/otsi/retail/ticketservice/controller/TicketController.java
+++ src/main/java/com/otsi/retail/ticketservice/controller/TicketController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -40,7 +41,7 @@ public class TicketController {
 	}
 
 	@GetMapping(CommonRequestMappings.GET_TICKETS_BY_STATUS)
-	public GateWayResponse<?> getTickets(@RequestParam("status") TicketStatus status, @RequestParam Long clientId) {
+	public GateWayResponse<?> getTickets(@RequestParam("status") TicketStatus status, @RequestHeader Long clientId) {
 
 		List<Ticket> tickets = ticketService.getTicketsByStatus(status, clientId);
 
