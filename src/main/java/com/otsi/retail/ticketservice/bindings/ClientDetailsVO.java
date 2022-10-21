6aa37c1c6f091f854c4c2716f75f4b610c961dfd
Sudheer <sudheer.swamy@otsi.co.in>
2022-10-21 13:03:32 +0530

    changes in ClientDetailsVO

--- src/main/java/com/otsi/retail/ticketservice/bindings/ClientDetailsVO.java
+++ src/main/java/com/otsi/retail/ticketservice/bindings/ClientDetailsVO.java
@@ -1,6 +1,7 @@
 package com.otsi.retail.ticketservice.bindings;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.otsi.retail.ticketservice.common.PlanDetails;
@@ -15,48 +16,48 @@ import lombok.ToString;
 @AllArgsConstructor
 @NoArgsConstructor
 public class ClientDetailsVO {
-	
+
 	private Long id;
-	
+
 	private String name;
-	
+
 	private String organizationName;
-	
+
 	private String address;
-	
+
 	private LocalDate createdDate;
-	
+
 	private LocalDate lastModifiedDate;
-	
+
 	private String mobile;
-	
+
 	private String email;
-	
+
 	private boolean isActive;
 
-	
 	private Long createdBy;
-	
+
 	private Boolean isEsSlipEnabled;
-	
+
 	private Boolean isTaxIncluded;
-	
+
 	private String planName;
-	
+
 	private Long planId;
-	
+
 	private String description;
-	
-	private String  planTenure;
-	
+
+	private String planTenure;
+
 	private String rayzorPayPaymentId;
-	
+
 	private Long amount;
-	
+
 	@JsonIgnore
 	private PlanDetails plandetails;
-	
-	
-	
-	
+
+	private LocalDateTime planExpiryDate;
+
+	private LocalDateTime planActivatedDate;
+
 }
